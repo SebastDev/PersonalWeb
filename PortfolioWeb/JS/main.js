60ce9ed331c95ffa707a22af2abb534342e46a4c
SebastianCode <53224121+SebastianPrietoCode@users.noreply.github.com>
2021-08-29 20:53:20 -0500

    Add Se agrega el sitio web

---
+++ PortfolioWeb/JS/main.js
@@ -0,0 +1,8 @@
+function mostrar() {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+        footer: '<a href="">Why do I have this issue?</a>'
+      })      
+}
